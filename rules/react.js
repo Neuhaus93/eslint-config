65c11e9572051c93500c6b9d281114e0522df98c
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2022-12-14 13:25:53 -0300

    Fixing eslint

---
+++ rules/react.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/jsx-runtime",
+    ],
+    "settings": {
+        "react": {
+            "version": "detect",
+        },
+    },
+};
